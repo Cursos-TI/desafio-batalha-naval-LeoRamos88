c753dd33577f56bd59347df6b0a2b9c268160eb2
LEO RAMOS <leonardoramos1234560@gmail.com>
2024-12-19 01:09:28 +0000

    Projeto batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,166 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define LINHA 3
+#define COLUNA 5
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main()
+{
+    printf("Saída em cone :\n");
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int matriz[LINHA][COLUNA] = {0};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int percorrer = 1;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
+    for (int l = 0; l < LINHA; l++)
+    {
+        for (int c = 0; c < COLUNA; c++)
+        {
+            if (matriz[l][c] == matriz[0][2])
+            {
+                matriz[0][2] = 1;//PRIMEIRA LINHA
+            }
+
+            else if (matriz[l][c] == matriz[1][2])
+            {
+                for(int mudar = 1; mudar < 2;mudar++)
+                {
+                    for(int c = 1;c < 4;c++ )
+                    {
+                        matriz[mudar][c] = percorrer;
+
+
+                    }
+
+                }
+
+            }
+            else if(matriz[l][c] == matriz[2][0])
+            {
+                for (int l = 2; l < LINHA; l++)
+                {
+                    for (int c = 0; c< COLUNA; c++)
+                    {
+                        matriz[l][c] = percorrer;
+                    }
+
+                }
+
+            }
+
+            printf("%d",matriz[l][c]);
+
+        }
+        printf("\n");
+
+    }
+
+    //Octeadro
+    printf("\n");
+
+    printf("Octeadro : \n");
+
+
+    //RESETANDO A MINHA MATRIZ 
+
+    for(int l = 0;l <LINHA;l++)
+    {
+        for (int c = 0; c < COLUNA; c++)
+        {
+            matriz[l][c] = 0;
+        }
+    }
+
+    for (int l = 0; l < LINHA; l++)
+    {
+        for (int c = 0; c < COLUNA; c++)
+        {
+            if (matriz[l][c] == matriz[0][2])
+            {
+                matriz[0][2] = 1;//PRIMEIRA LINHA
+            }
+
+            else if (matriz[l][c] == matriz[1][2])
+            {
+                for(int mudar = 1; mudar < 2;mudar++)
+                {
+                    for(int c = 1;c < 4;c++ )
+                    {
+                        matriz[mudar][c] = percorrer;
+
+
+                    }
+
+                }
+
+            }
+            else if(matriz[l][c] == matriz[2][2])
+            {
+                matriz[2][2] = percorrer;
+            }
+
+            printf("%d",matriz[l][c]);
+
+        }
+        printf("\n");
+
+
+    }
+
+    printf("\n");
+
+    printf("CRUZ : \n");
+
+    //RESETANDO A MINHA MATRIZ 
+
+    for(int l = 0;l <LINHA;l++)
+    {
+        for (int c = 0; c < COLUNA; c++)
+        {
+            matriz[l][c] = 0;
+        }
+    }
+
+    for (int l = 0; l < LINHA; l++)
+    {
+        for (int c = 0; c < COLUNA; c++)
+        {
+            if (matriz[l][c] == matriz[0][2])
+            {
+                matriz[0][2] = 1;//PRIMEIRA LINHA
+            }
+
+            else if (matriz[l][c] == matriz[1][0])
+            {
+                for(int mudar = 1; mudar < 2;mudar++)
+                {
+                    for(int c = 0;c < 5;c++ )
+                    {
+                        matriz[mudar][c] = percorrer;
+
+
+                    }
+
+                }
+
+            }
+            else if(matriz[l][c] == matriz[2][2])
+            {
+                 matriz[2][2] = percorrer;
+
+            }
+
+            printf("%d",matriz[l][c]);
+
+        }
+        printf("\n");
+
+
+    }
+
+
+
 
     return 0;
 }
